68639a9eae47a4df725eb656b0499aef6ae24193
johnietre <johnietrebus@gmail.com>
2021-01-27 01:20:41 -0600

    Added to API

--- server.go
+++ server.go
@@ -1,253 +1,141 @@
-// Update SELECT-LIKE stmt to handle
-  // Ex) if chico is in the db but not chica and you search for chica
-  // chico should come up
-  // Possibly use RegEx
-// Figure out why http.Error does not redirect to an error page
-// Finish "Not what you're looking for?" button/function
-  // After that, make it display "Still not what you're looking for?"
-// Possibly use sqlite user functions rather than my own
 package main
 
 import (
   "database/sql"
   "encoding/json"
+  "errors"
   "fmt"
   "html/template"
-  _ "github.com/mattn/go-sqlite3"
   "log"
-  "net"
   "net/http"
   "strings"
-  "sync" // Only going to be used while sqlite is used
+  // "sync"
+
+  _ "github.com/mattn/go-sqlite3"
 )
 
-// Struct for word entries
-type entry struct {
-  Word string
-  Def string
-  Gender string
-  Action string // For API
+// Entry holds information for a word
+type Entry struct {
+  Word string `json:"word"`
+  Def string `json:"def"`
+  Gender string `json:"gender"`
 }
 
-// Struct containing the page data (used for HTML page and API data)
-type PageData struct {
-  Msg string
-  Entries []entry
-  NotFound bool
+type Error struct {
+  Error string `json:"error"`
 }
 
 var (
-  IP string = "192.168.1.130"
-  // IP string = "localhost"
-  WebPort string = ":8000"
-  APIPort string = ":9000"
-  lock sync.Mutex
+  // ip string = "129.119.172.61"
+  ip string = "localhost"
+  port string = "8000"
+  dbPath string = "dict.db"
+  db *sql.DB
+  temp *template.Template
 )
 
-func WebpageServer() {
-  fs := http.FileServer(http.Dir("./static/"))
-  http.Handle("/static/", http.StripPrefix("/static", fs))
-  http.HandleFunc("/", PageHandler)
-  log.Fatal(http.ListenAndServe(IP + WebPort, nil))
-}
-
 func main() {
-  log.Printf("Running on %s%s (API %s)", IP, WebPort, APIPort)
-  WebpageServer()
-  go APIServer()
-}
-
-func checkErr(err error) bool {
+  var err error
+  db, err = sql.Open("sqlite3", dbPath)
   if err != nil {
-    log.Println(err)
-    return true // Return true if there was an error
-  } else {
-    return false
-  }
-}
-
-func getDB() *sql.DB {
-  db, err := sql.Open("sqlite3", "/Users/johnierodgers/.databases/dict.db")
-  if checkErr(err) {
-    return nil
-  }
-  return db
-}
-
-func getPage(find string, like bool) PageData {
-  var page PageData
-  // If we are finding "LIKE" words, then the original word wasn't found
-  page.NotFound = like
-  db := getDB()
-  if db == nil {
-    page.Msg = "Database error"
-    return page
+    log.Panic(err)
   }
   defer db.Close()
-  var stmt string
-  if like { // Use this stmt if we are trying to "LIKE" words
-    stmt = fmt.Sprintf(`SELECT * FROM words WHERE word LIKE "%%%s%%" ORDER BY word`, find)
-  } else {
-    if find == "*" { // Use this stmt if we want to get all words in the dictionary
-      stmt = `SELECT * FROM words ORDER BY word`
-    } else { // Use this if we are looking for a specific word
-      stmt = fmt.Sprintf(`SELECT * FROM words WHERE word="%s" ORDER BY word`, find)
-    }
+
+  parse()
+
+  fs := http.FileServer(http.Dir("templates"))
+  http.Handle("/static/", http.StripPrefix("/static", fs))
+  http.HandleFunc("/", pageHandler)
+  http.HandleFunc("/api", apiHandler)
+  log.Panic(http.ListenAndServe(ip + ":" + port, nil))
+}
+
+func parse() {
+  temp = template.Must(template.ParseFiles("templates/index.html"))
+}
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+  temp.Execute(w, nil)
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+  if err := r.ParseForm(); err != nil {
+    log.Println(err)
+    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+    return
   }
-  rows, err := db.Query(stmt) // Execute the stmt and get the rows
-  if checkErr(err) {
-    page.Msg = "Database SELECT error"
-    return page
+  w.Header()["Content-Type"] = []string{"application/json"}
+  encoder := json.NewEncoder(w)
+  word := strings.Join(r.Form["word"], " ")
+  if word == "" {
+    if err := encoder.Encode(Error{"must provide word"}); err != nil {
+      log.Println(err)
+    }
+    return
+  }
+  if r.Method == http.MethodPost {
+    method := r.FormValue("method")
+    oldDef := strings.Join(r.Form["old"], " ")
+    newDef := strings.Join(r.Form["new"], " ")
+    if method == "suggestion" {
+      /* Handle suggestions */
+    } else if method == "admin" {
+      email, password := r.FormValue("email"), r.FormValue("password")
+      row := db.QueryRow(fmt.Sprintf(`SELECT * FROM users WHERE email="%s"`, email))
+      var e, p string
+      if err := row.Scan(&e, &p); errors.Is(err, sql.ErrNoRows) {
+        if err := encoder.Encode(Error{"invalid username or password"}); err != nil {
+          log.Println(err)
+        }
+        return
+      } else if err != nil {
+        log.Println(err)
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+      }
+      /* Hash password */
+      if p != password {
+        if err := encoder.Encode(Error{"invalid username or password"}); err != nil {
+          log.Println(err)
+        }
+        return
+      }
+      /* Create, Update, or Delete word */
+      println(oldDef, newDef)
+    } else {
+      if err := encoder.Encode(Error{"invalid method"}); err != nil {
+        log.Println(err)
+        return
+      }
+    }
+    return
+  }
+  stmt := `SELECT * FROM words`
+  if word == "*" {
+    stmt += fmt.Sprintf(` ORDER BY word`)
+  } else {
+      stmt += fmt.Sprintf(` WHERE word="%s" ORDER BY def`, word)
+  }
+  rows, err := db.Query(stmt)
+  if err != nil {
+    log.Println(err)
+    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+    return
   }
   defer rows.Close()
-  var word, def, gender string
-  for rows.Next() { // Loop through the rows, appending them to the page entries
-    if !checkErr(rows.Scan(&word, &def, &gender)) {
-      page.Entries = append(page.Entries, entry{word, def, gender, ""})
-    }
-  }
-  return page
-}
-
-func addWord(word, def, gender string) {
-  lock.Lock() // Lock the lock so the database can be safely written to
-  defer lock.Unlock()
-  db := getDB()
-  if db == nil {
-    return
-  }
-  defer db.Close()
-  var stmt string
-  stmt = fmt.Sprintf(`INSERT INTO words VALUES ("%s", "%s", "%s")`, word, def, gender)
-  _, err := db.Exec(stmt)
-  checkErr(err)
-}
-
-func hash(passwor string, login bool) string {
-  return "hashed"
-}
-
-func register(fname, lname, username, email, password string) {
-  db := getDB()
-  if db == nil {
-    return
-  }
-  defer db.Close()
-  var stmt string
-  stmt = fmt.Sprintf(`SELECT * FROM users WHERE email="%s"`)
-  rows, err := db.Query(stmt)
-  if checkErr(err) {
-    return
-  }
+  var def, gender string
+  var entries []Entry
   for rows.Next() {
-    println("Email address already used")
-    rows.Close()
-    return
-  }
-  hash(password, false)
-  return
-}
-
-func login(username, email, password string) {
-  return
-}
-
-func PageHandler(w http.ResponseWriter, r *http.Request) {
-  var page PageData
-  query := r.URL.Query()
-  if r.Method == http.MethodGet {
-    if len(query) != 0 { // If the page was not just loaded (a query was made)
-      var word string
-      if _, exists := query["all"]; exists == true {
-        word = "*"
-      } else {
-        word = strings.ToLower(query["word"][0])
-      }
-      page = getPage(word, false)
-      if len(page.Entries) == 0 && word != "*" && page.Msg == "" {
-        page = getPage(word, true)
-        if len(page.Entries) == 0 && page.Msg == "" {
-          page = PageData{Msg: "No matches found", NotFound: true,}
-        } else {
-          page.Msg = `Could not find any mathces for "` + word + `"`
-        }
-      }
-    } else { // Page was just loaded (no queries have been made)
-      page.Msg = "Search for words or phrases"
+    if err := rows.Scan(&word, &def, &gender); err != nil {
+      log.Println(err)
+      http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+      return
     }
-  } else { // The user is adding a word to the dictionary
-    if r.FormValue("submit") == "Add" { // If they click the "Add" button
-      var word string = strings.ToLower(r.FormValue("word"))
-      var def string = r.FormValue("def")
-      var gender string = r.FormValue("gender")
-      addWord(word, def, gender)
-      page = getPage(word, false)
-      if page.Msg == "" {
-        page.Msg = "Word/Phrase added!"
-      }
-    } else { // If they click the "Cancel" button
-      page = PageData{Msg: "Search for words or phrases"}
-    }
+    entries = append(entries, Entry{word, def, gender})
   }
-  ts, err := template.ParseFiles("../templates/index.html")
-  if err != nil {
+  if err := encoder.Encode(entries); err != nil {
     log.Println(err)
-    http.Error(w, "Internal Server Error... My bad", 500)
-    return
-  }
-  if err = ts.Execute(w, page); err != nil {
-    log.Println(err)
-    http.Error(w, "Internal Server Error... My bad", 500)
-  }
-}
-
-/* Server API for the app */
-
-func APIServer() {
-  // Create the socket listener
-  ln, err := net.Listen("tcp", IP + APIPort)
-  if err != nil {
-    log.Panicln(err)
-  }
-  for {
-    // Accept connections
-    // Possibly use goroutine after accepting connenction to handle for things
-    conn, err := ln.Accept()
-    if checkErr(err) {
-      conn.Close()
-      continue
-    }
-    // Read the message and unmarshal it into an entry struct
-    var msg []byte
-    if _, err := conn.Read(msg); checkErr(err) {
-      conn.Close()
-      continue
-    }
-    var e *entry = &entry{}
-    if checkErr(json.Unmarshal(msg, e)) {
-      conn.Close()
-      continue
-    }
-    if e.Action == "get" { // Handle queries for data
-      var page PageData
-      var word string = strings.ToLower(e.Word)
-      page = getPage(word, false)
-      if len(page.Entries) == 0 && word != "*" && page.Msg == "" {
-        page = getPage(word, true)
-        if len(page.Entries) == 0 && page.Msg == "" {
-          page = PageData{Msg: "No matches found", NotFound: true,}
-        } else {
-          page.Msg = "Could not find any mathces for " + word
-        }
-      }
-      pb, err := json.Marshal(&page)
-      if checkErr(err) {
-        _, err = conn.Write(pb)
-        checkErr(err)
-      }
-    } else { // Handle adding words
-      addWord(e.Word, e.Def, e.Gender)
-    }
-    conn.Close()
+    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
   }
 }
